59d4f55a56011062bcf81514469147e5c34d5684
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-03-03 10:24:10 -0600

    add npm packages. add file structure. add basic database structure.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO employees (employeeName)
+VALUES ("Mamrie"),
+       ("Grace"),
+       ("Hannah"),
+       ("Billie"),
+       ("Kristen"),
+       ("Chelsea"),
+       ("Liz");
+
+-- TODO: add seeds for departments and roles tables
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mysql = require('mysql2');
+const printTable = require('console-table-printer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'business_db'
+  },
+  console.log(`Connected to the business_db database.`)
+);
+
+// TODO: add CRUD routes
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
