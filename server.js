4825d9d70351757591c1472ff4335b50a4b7ac86
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-03-11 10:29:50 -0600

    remove sequelizenpm package (functionality not needed). add save method to classes. add employee CRUD routes.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const { printTable } = require('console-table-printer');
 
-const sequelize = require('./config/connection');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,10 +11,92 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// TODO: add CRUD routes
-
-
-// Connect to database and start server
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// CRUD routes
+// Create new employee
+app.post('/api/new-employee', ({ body }, res) => {
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?,?,?,?)`;
+  const params = [body.first_name,body.last_name,body.role_id,body.manager_id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
 });
+
+// Read all employees
+app.get('/api/employees', (req, res) => {
+  const sql = `SELECT * FROM employee`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Update Employee
+app.put('/api/employee/:id', (req, res) => {
+  const sql = `UPDATE employee SET employee = ? WHERE id = ?`;
+  const params = [req.body.employee, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+
+// Delete Employee
+app.delete('/api/employee/:id', (req, res) => {
+  const sql = `DELETE FROM employee WHERE id = ?`;
+  const params = [req.params.id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+      message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server
+app.listen(PORT, () => 
+  console.log('Now listening')
+);
